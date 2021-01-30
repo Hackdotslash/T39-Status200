294f1b5997bafd6b16baadada874a92e49493914
Anushk Gautam <me190003012@iiti.ac.in>
2021-01-30 11:59:35 +0530

    Add React Component

---
+++ src/components/Login/Login.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Login.css'
+
+function Login() {
+    return (
+        <div classname="login">
+            <button onClick={alert("DONE")}>
+                Login With Google
+            </button>
+        </div>
+    )
+}
+
+export default Login;
---
+++ src/firebase.js
@@ -0,0 +1,20 @@
+//Secret data!
+import firebase from "firebase";
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCHC917vxtaIBKzP6CJZGq66_fW1Qo3vQE",
+    authDomain: "oecd-6e54a.firebaseapp.com",
+    projectId: "oecd-6e54a",
+    storageBucket: "oecd-6e54a.appspot.com",
+    messagingSenderId: "697205554597",
+    appId: "1:697205554597:web:699692f7a9d1ab63033c4c",
+    measurementId: "G-WSW3800633"
+  };
+
+ const firebaseApp = firebase.initializeApp(firebaseConfig);
+ 
+ const db = firebaseApp.firestore();
+ const auth = firebase.auth();
+
+ export { db , auth } ;
