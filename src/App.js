d3afdcc07372e0634b1de5a5c0884aea3422b209
Anushk Gautam <me190003012@iiti.ac.in>
2021-01-30 10:42:35 +0530

    Installed React-App, Firebase, Material-UI

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     Online Cheating Detector
+    </div>
+  );
+}
+
+export default App;
+// online-cheating-detector
