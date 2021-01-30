f28a25d50cef54c0b85607013a18b597ee2e8534
Kuldeep28-star <kuldeepsingh281999@gmail.com>
2021-01-30 16:47:06 +0530

    home section

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
 import React from 'react';
-import './App.css';
-import Login from './components/Login/Login';
+import GlobalStyle from './globalStyles';
+import Home from './pages/HomePage/Home';
+import Services from './pages/Services/Services';
+import Products from './pages/Products/Products';
+import SignUp from './pages/SignUp/SignUp';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import ScrollToTop from './components/ScrollToTop';
+import { Navbar, Footer } from './components';
 
 function App() {
   return (
-    <div className="App">
-     Online Cheating Detector
-     <Login/>
-    </div>
+    <Router>
+      <GlobalStyle />
+      <ScrollToTop />
+      <Navbar />
+      <Switch>
+        <Route path='/' exact component={Home} />
+        <Route path='/services' component={Services} />
+        <Route path='/products' component={Products} />
+        <Route path='/sign-up' component={SignUp} />
+      </Switch>
+      <Footer />
+    </Router>
   );
 }
 
 export default App;
-// online-cheating-detector
